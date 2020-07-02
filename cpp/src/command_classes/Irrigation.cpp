55b22cde87c62cb37228257c35753134bce2d379
Kevin Oberlies <favilo@gmail.com>
2020-07-02 11:58:03 -0700

    fixing issues that came up when namespaces were added

--- cpp/src/command_classes/Irrigation.cpp
+++ cpp/src/command_classes/Irrigation.cpp
@@ -15,167 +15,174 @@
 
 #include "value_classes/ValueBool.h"
 
-using namespace OpenZWave;
-
-enum IrrigationCmd
+namespace OpenZWave
 {
-	IrrigationCmd_SystemInfoGet			= 0x01,
-	IrrigationCmd_SystemInfoReport  	= 0x02,
-	IrrigationCmd_SystemStatusGet		= 0x03,
-	IrrigationCmd_SystemStatusReport	= 0x04,
-	IrrigationCmd_SystemConfigSet		= 0x05,
-	IrrigationCmd_SystemConfigGet  		= 0x06,
-	IrrigationCmd_SystemConfigReport	= 0x07,
-
-	IrrigationCmd_ValveInfoGet			= 0x08,
-	IrrigationCmd_ValveInfoReport		= 0x09,
-
-	IrrigationCmd_ValveConfigSet		= 0x0A,
-	IrrigationCmd_ValveConfigGet		= 0x0B,
-	IrrigationCmd_ValveConfigReport		= 0x0C,
-
-	IrrigationCmd_ValveRun				= 0x0D,
-
-	IrrigationCmd_ValveTableSet			= 0x0E,
-	IrrigationCmd_ValveTableGet			= 0x0F,
-	IrrigationCmd_ValveTableReport		= 0x10,
-	IrrigationCmd_ValveTableRun			= 0x11,
-
-	IrrigationCmd_Shutoff				= 0x12
-};
-
-enum {
-	// System Info
-	IrrigationIndex_MasterValvePresent = 1,
-	IrrigationIndex_TotalValves,
-	IrrigationIndex_TotalValveTables,
-	IrrigationIndex_MaxValveTableSize,
-
-	// System Status
-	IrrigationIndex_SystemVoltage,
-
-	// Sensor Status bits
-	// 0 = Flow Sensor Detected
-	// 1 = Pressure Sensor Detected
-	// 2 = Rain Sensor Detected
-	// 3 = Moisture Sensor Detected
-	// 4–7 = Reserved
-	IrrigationIndex_SensorStatusBitSet,
-
-
-	IrrigationIndex_Flow,
-	IrrigationIndex_Pressure,
-	IrrigationIndex_ShutoffDuration,
-
-	// System Error Status bits
-	// 0 = device has not been programmed
-	// 1 = device has experienced emergency shutdown
-	// 2 = system pressure high threshold has been triggered
-	// 3 = system pressure low threshold has been triggered
-	// 4 = valve (master or zone) is reporting errors. Valves can be checked with IrrigationCmd_ValveInfoGet
-	// 5-7 = Reserved
-	IrrigationIndex_SystemErrorStatus,
-	IrrigationIndex_MasterValveOn,
-	IrrigationIndex_ValveIdOn,
-
-	// System Config
-	IrrigationIndex_MasterValveDelay,
-	IrrigationIndex_HighPressureThreshold,
-	IrrigationIndex_LowPressureThreshold,
-	// Sensor Polarity Bits
-	// 0 = Rain Sensor Polarity (0 LOW, 1 HIGH)
-	// 1 = Moisture Sensor Polarity (0 LOW, 1 HIGH)
-	// 2-6 = Reserved
-	// 7 = Valid
-	IrrigationIndex_SensorPolarity,
-
-	IrrigationIndex_ValveInfoStart = 0x100,
-	IrrigationIndex_ValveConfigStart = 0x200,
-
-};
-
-//-----------------------------------------------------------------------------
-// <Irrigation::RequestState>
-// Request current state from the device
-//-----------------------------------------------------------------------------
-bool Irrigation::RequestState
-(
-		uint32 const _requestFlags,
-		uint8 const _instance,
-		Driver::MsgQueue const _queue
-	)
-{
-	// XXX: Fix this
-	if( _requestFlags & RequestFlag_Dynamic )
+	namespace Internal
 	{
-		return RequestValue( _requestFlags, 0, _instance, _queue );
+		namespace CC
+		{
+
+			enum IrrigationCmd
+			{
+				IrrigationCmd_SystemInfoGet			= 0x01,
+				IrrigationCmd_SystemInfoReport  	= 0x02,
+				IrrigationCmd_SystemStatusGet		= 0x03,
+				IrrigationCmd_SystemStatusReport	= 0x04,
+				IrrigationCmd_SystemConfigSet		= 0x05,
+				IrrigationCmd_SystemConfigGet  		= 0x06,
+				IrrigationCmd_SystemConfigReport	= 0x07,
+
+				IrrigationCmd_ValveInfoGet			= 0x08,
+				IrrigationCmd_ValveInfoReport		= 0x09,
+
+				IrrigationCmd_ValveConfigSet		= 0x0A,
+				IrrigationCmd_ValveConfigGet		= 0x0B,
+				IrrigationCmd_ValveConfigReport		= 0x0C,
+
+				IrrigationCmd_ValveRun				= 0x0D,
+
+				IrrigationCmd_ValveTableSet			= 0x0E,
+				IrrigationCmd_ValveTableGet			= 0x0F,
+				IrrigationCmd_ValveTableReport		= 0x10,
+				IrrigationCmd_ValveTableRun			= 0x11,
+
+				IrrigationCmd_Shutoff				= 0x12
+			};
+
+			enum {
+				// System Info
+				IrrigationIndex_MasterValvePresent = 1,
+				IrrigationIndex_TotalValves,
+				IrrigationIndex_TotalValveTables,
+				IrrigationIndex_MaxValveTableSize,
+
+				// System Status
+				IrrigationIndex_SystemVoltage,
+
+				// Sensor Status bits
+				// 0 = Flow Sensor Detected
+				// 1 = Pressure Sensor Detected
+				// 2 = Rain Sensor Detected
+				// 3 = Moisture Sensor Detected
+				// 4–7 = Reserved
+				IrrigationIndex_SensorStatusBitSet,
+
+
+				IrrigationIndex_Flow,
+				IrrigationIndex_Pressure,
+				IrrigationIndex_ShutoffDuration,
+
+				// System Error Status bits
+				// 0 = device has not been programmed
+				// 1 = device has experienced emergency shutdown
+				// 2 = system pressure high threshold has been triggered
+				// 3 = system pressure low threshold has been triggered
+				// 4 = valve (master or zone) is reporting errors. Valves can be checked with IrrigationCmd_ValveInfoGet
+				// 5-7 = Reserved
+				IrrigationIndex_SystemErrorStatus,
+				IrrigationIndex_MasterValveOn,
+				IrrigationIndex_ValveIdOn,
+
+				// System Config
+				IrrigationIndex_MasterValveDelay,
+				IrrigationIndex_HighPressureThreshold,
+				IrrigationIndex_LowPressureThreshold,
+				// Sensor Polarity Bits
+				// 0 = Rain Sensor Polarity (0 LOW, 1 HIGH)
+				// 1 = Moisture Sensor Polarity (0 LOW, 1 HIGH)
+				// 2-6 = Reserved
+				// 7 = Valid
+				IrrigationIndex_SensorPolarity,
+
+				IrrigationIndex_ValveInfoStart = 0x100,
+				IrrigationIndex_ValveConfigStart = 0x200,
+
+			};
+
+			//-----------------------------------------------------------------------------
+			// <Irrigation::RequestState>
+			// Request current state from the device
+			//-----------------------------------------------------------------------------
+			bool Irrigation::RequestState
+			(
+					uint32 const _requestFlags,
+					uint8 const _instance,
+					Driver::MsgQueue const _queue
+				)
+			{
+				// XXX: Fix this
+				if( _requestFlags & RequestFlag_Dynamic )
+				{
+					return RequestValue( _requestFlags, 0, _instance, _queue );
+				}
+
+				return false;
+			}
+
+			bool Irrigation::RequestValue
+			(
+				uint32 const _requestFlags,
+				uint8 const _dummy1,	// = 0 (not used)
+				uint8 const _instance,
+				Driver::MsgQueue const _queue
+			)
+			{
+
+				// XXX: implement
+				return false;
+			}
+
+			bool Irrigation::HandleMsg
+			(
+					const uint8* _data,
+					const uint32 _length,
+					const uint32 _instance
+			)
+			{
+				// XXX: implement
+				return false;
+			}
+
+			bool Irrigation::SetValue
+			(
+					const Value& _value
+			)
+			{
+				// XXX: implement
+				return false;
+			}
+
+			void Irrigation::CreateVars
+			(
+					const uint8 _instance
+			)
+			{
+				if( Node* node = GetNodeUnsafe() )
+				{
+					node->CreateValueBool(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_MasterValvePresent, "Master Valve Present", "", true, false, false, 0);
+					node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_TotalValves, "Total Number of Valves", "", true, false, 0, 0);
+					node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_TotalValveTables, "Total Number of Valve Tables", "", true, false, 0, 0);
+					node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_MaxValveTableSize, "Valve Table Max Size", "", true, false, 0, 0);
+
+					node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_SystemVoltage, "System Voltage", "V", true, false, 0, 0);
+					node->CreateValueBitSet(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_SensorStatusBitSet, "Sensor Status", "", true, false, 0, 0);
+					node->CreateValueDecimal(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_Flow, "Flow Reading", "l/h", true, false, "0.0", 0);
+					node->CreateValueDecimal(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_Pressure, "Pressure Reading", "kPa", true, false, "0.0", 0);
+					node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_ShutoffDuration, "Shutoff Duration", "h", true, false, 0, 0);
+					node->CreateValueBitSet(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_SystemErrorStatus, "System Error Status", "", true, false, 0, 0);
+					node->CreateValueBool(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_MasterValveOn, "Master Valve On", "", true, false, false, 0);
+					node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_ValveIdOn, "Valve ID On", "", true, false, false, 0);
+
+					node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_MasterValveDelay, "Master Valve Delay", "s", true, false, 0, 0);
+					node->CreateValueDecimal(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_HighPressureThreshold, "High Pressure Threshold", "kPa", true, false, "0.0", 0);
+					node->CreateValueDecimal(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_LowPressureThreshold, "Low Pressure Threshold", "kPa", true, false, "0.0", 0);
+					node->CreateValueBitSet(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_SensorPolarity, "Sensor Polarity", "", true, false, 0, 0);
+
+					// Don't make values for the valve-info or tables. There can potentially be up to 256 of each.
+					// We can do that when reading/writing to them on the device. Saving space in the indexes though
+				}
+			}
+
+		}
 	}
-
-	return false;
 }
-
-bool Irrigation::RequestValue
-(
-	uint32 const _requestFlags,
-	uint8 const _dummy1,	// = 0 (not used)
-	uint8 const _instance,
-	Driver::MsgQueue const _queue
-)
-{
-
-	// XXX: implement
-	return false;
-}
-
-bool Irrigation::HandleMsg
-(
-		const uint8* _data,
-		const uint32 _length,
-		const uint32 _instance
-)
-{
-	// XXX: implement
-	return false;
-}
-
-bool Irrigation::SetValue
-(
-		const Value& _value
-)
-{
-	// XXX: implement
-	return false;
-}
-
-void Irrigation::CreateVars
-(
-		const uint8 _instance
-)
-{
-	if( Node* node = GetNodeUnsafe() )
-	{
-		node->CreateValueBool(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_MasterValvePresent, "Master Valve Present", "", true, false, false, 0);
-		node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_TotalValves, "Total Number of Valves", "", true, false, 0, 0);
-		node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_TotalValveTables, "Total Number of Valve Tables", "", true, false, 0, 0);
-		node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_MaxValveTableSize, "Valve Table Max Size", "", true, false, 0, 0);
-
-		node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_SystemVoltage, "System Voltage", "V", true, false, 0, 0);
-		node->CreateValueBitSet(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_SensorStatusBitSet, "Sensor Status", "", true, false, 0, 0);
-		node->CreateValueDecimal(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_Flow, "Flow Reading", "l/h", true, false, "0.0", 0);
-		node->CreateValueDecimal(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_Pressure, "Pressure Reading", "kPa", true, false, "0.0", 0);
-		node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_ShutoffDuration, "Shutoff Duration", "h", true, false, 0, 0);
-		node->CreateValueBitSet(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_SystemErrorStatus, "System Error Status", "", true, false, 0, 0);
-		node->CreateValueBool(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_MasterValveOn, "Master Valve On", "", true, false, false, 0);
-		node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_ValveIdOn, "Valve ID On", "", true, false, false, 0);
-
-		node->CreateValueByte(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_MasterValveDelay, "Master Valve Delay", "s", true, false, 0, 0);
-		node->CreateValueDecimal(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_HighPressureThreshold, "High Pressure Threshold", "kPa", true, false, "0.0", 0);
-		node->CreateValueDecimal(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_LowPressureThreshold, "Low Pressure Threshold", "kPa", true, false, "0.0", 0);
-		node->CreateValueBitSet(ValueID::ValueGenre_User, GetCommandClassId(), _instance, IrrigationIndex_SensorPolarity, "Sensor Polarity", "", true, false, 0, 0);
-
-		// Don't make values for the valve-info or tables. There can potentially be up to 256 of each.
-		// We can do that when reading/writing to them on the device. Saving space in the indexes though
-	}
-}
-
-
--- cpp/src/command_classes/Irrigation.h
+++ cpp/src/command_classes/Irrigation.h
@@ -12,33 +12,41 @@
 
 namespace OpenZWave {
 
-	class ValueByte; // XXX: Do we need this???
+	namespace Internal
+	{
+		namespace CC
+		{
 
-	/** \brief Implements COMMAND_CLASS_IRRIGATION (0x6B), a Z-Wave device command class.
-	 */
-	class Irrigation: public CommandClass {
-	public:
-		static CommandClass* Create( uint32 const _homeId, uint8 const _nodeId ) { return new Irrigation(_homeId, _nodeId); }
-		virtual ~Irrigation(){}
+			class ValueByte; // XXX: Do we need this???
 
-		static uint8 const StaticGetCommandClassId(){ return 0x6B; }
-		static string const StaticGetCommandClassName(){ return "COMMAND_CLASS_IRRIGATION"; }
+			/** \brief Implements COMMAND_CLASS_IRRIGATION (0x6B), a Z-Wave device command class.
+			*/
+			class Irrigation: public CommandClass
+			{
+				public:
+					static CommandClass* Create( uint32 const _homeId, uint8 const _nodeId ) { return new Irrigation(_homeId, _nodeId); }
+					virtual ~Irrigation(){}
 
-		// From CommandClass
-		virtual bool RequestState( uint32 const _requestFlags, uint8 const _instance, Driver::MsgQueue const _queue );
-		virtual bool RequestValue( uint32 const _requestFlags, uint8 const _index, uint8 const _instance, Driver::MsgQueue const _queue );
-		virtual uint8 const GetCommandClassId()const{ return StaticGetCommandClassId(); }
-		virtual string const GetCommandClassName()const{ return StaticGetCommandClassName(); }
-		virtual bool HandleMsg( uint8 const* _data, uint32 const _length, uint32 const _instance = 1 );
-		virtual bool SetValue( Value const& _value );
+					static uint8 const StaticGetCommandClassId(){ return 0x6B; }
+					static string const StaticGetCommandClassName(){ return "COMMAND_CLASS_IRRIGATION"; }
 
-	protected:
-		virtual void CreateVars( uint8 const _instance );
+					// From CommandClass
+					virtual bool RequestState( uint32 const _requestFlags, uint8 const _instance, Driver::MsgQueue const _queue );
+					virtual bool RequestValue( uint32 const _requestFlags, uint8 const _index, uint8 const _instance, Driver::MsgQueue const _queue );
+					virtual uint8 const GetCommandClassId()const{ return StaticGetCommandClassId(); }
+					virtual string const GetCommandClassName()const{ return StaticGetCommandClassName(); }
+					virtual bool HandleMsg( uint8 const* _data, uint32 const _length, uint32 const _instance = 1 );
+					virtual bool SetValue( Value const& _value );
 
-	private:
-		Irrigation(uint32 const _homeId, uint8 const _nodeId): CommandClass( _homeId, _nodeId ) {}
-	};
+				protected:
+					virtual void CreateVars( uint8 const _instance );
 
+				private:
+					Irrigation(uint32 const _homeId, uint8 const _nodeId): CommandClass( _homeId, _nodeId ) {}
+			};
+
+		}
+	}
 } /* namespace OpenZWave */
 
 #endif /* _Irrigation_H_ */
